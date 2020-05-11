daf08ec502fe71e969be4b07ddadfb331fc07652
Ubuntu <ubuntu@ip-172-31-26-69.us-east-2.compute.internal>
2020-05-11 20:10:12 +0000

    updated essentials

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -1,9 +1,25 @@
-package 'apache2'
+case node[:platform]
+when "ubuntu", "debian"
+  package "apache2" do
+    action :install
+  end
+when "centos", "redhat", "amazon"
+  package "httpd" do
+    action :install
+  end
+end
+
+service 'apache2' do
+  case node[:platform]
+  when 'redhat', 'centos', 'amazon'
+    service_name 'httpd'
+  when 'debian', 'ubuntu'
+    service_name 'apache2'
+  end
+  action [:start, :enable]
+end
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
 end
 
-service 'apache2' do
-  action [:enable, :start]
-end
